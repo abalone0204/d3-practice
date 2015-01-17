57c80a4613bbb593673c87b88b6c3692814c3733
abalone0204 <abalone0204@gmail.com>
2015-01-17 16:31:11 +0800

    scale(1)

--- assets/javascripts/pages/enter.js
+++ assets/javascripts/pages/enter.js
@@ -3,7 +3,6 @@ var canvas = d3.select(".demo")
                .attr({
                 height: 300,
                 width: 300,
-                fill: "red"
                });
 data = d3.range(5);
 
--- assets/javascripts/pages/scale.js
+++ assets/javascripts/pages/scale.js
@@ -1,1 +1,57 @@
 d3.select("h2").text("Scale in d3.js");
+
+
+data = [2, 3, 4, 5]
+var svg = d3.select(".demo")
+  .append("svg")
+  .attr({
+    height: 200,
+    width: 200
+  });
+var heightScale = d3.scale.linear()
+  .domain([0, d3.max(data)])
+  .range([0, 200]);
+
+svg.selectAll("rect")
+  .data(data)
+  .enter()
+  .append("rect")
+  .attr({
+    x: function(d, i) {
+      return i * 53;
+    },
+    y: function(d, i) {
+      return 200 - d;
+    },
+    width: 50,
+    height: function(d) {
+      return d;
+    },
+    fill: "tomato"
+  });
+d3.select(".demo").append("br");
+var svg2 = d3.select(".demo")
+  .append("svg")
+  .attr({
+    height: 200,
+    width: 200
+  });
+
+
+svg2.selectAll("rect")
+  .data(data)
+  .enter()
+  .append("rect")
+  .attr({
+    x: function(d, i) {
+      return i * 53;
+    },
+    y: function(d, i) {
+      return 200 - heightScale(d);
+    },
+    width: 50,
+    height: function(d) {
+      return heightScale(d);
+    },
+    fill: "lightblue"
+  });
