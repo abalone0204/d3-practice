ec0a8b761f8eec23919871acd70cea364c2de0b6
abalone0204 <abalone0204@gmail.com>
2015-01-17 15:00:48 +0800

    done enter and exit()

--- assets/javascripts/pages/enter.js
+++ assets/javascripts/pages/enter.js
@@ -6,3 +6,27 @@ var canvas = d3.select(".demo")
                 fill: "red"
                });
 data = d3.range(5);
+
+canvas.selectAll("rect")
+      .data(data)
+      .enter()
+        .append("rect")
+          .attr({
+            x: function(d,i){ return i*40},
+            y: 150,
+            width: 30,
+            height: 30,
+            fill: "#444"
+          });
+anotherData = d3.range(3);
+
+canvas.selectAll("rect")
+      .data(anotherData)
+      .exit()
+          .attr({
+            x: function(d,i){ return i*40},
+            y: 150,
+            width: 30,
+            height: 30,
+            fill: "lightblue"
+          });
