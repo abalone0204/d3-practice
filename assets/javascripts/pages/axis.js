6956369e253f56bd344da66c816f59821f9f4d99
abalone0204 <abalone0204@gmail.com>
2015-01-18 15:43:54 +0800

    finish the axis lesson

--- assets/javascripts/pages/axis.js
+++ assets/javascripts/pages/axis.js
@@ -1,12 +1,12 @@
 d3.select("h2").text("Axis in D3.js");
 
-var w = 500,
+var w = 600,
   h = 300,
   margin={
-    left: 20,
+    left: 40,
     right: 20,
     bottom: 20,
-    top:20
+    top:40
   },
   radius =6,
   data = [];
@@ -27,11 +27,29 @@ var canvas = d3.select(".demo")
     height: h
   });
 var xScale = d3.scale.linear()
-               .domain([0, d3.max(data, function(d){ return d.x;})])
+               .domain([0, 100])
                .range([margin.left,w-margin.left]);
 var yScale = d3.scale.linear()
-               .domain([0, d3.max(data, function(d){ return d.y;})])
+               .domain([0, 100])
                .range([margin.top, h-margin.top]);
+// Axis
+
+var xAxis = d3.svg.axis().scale(xScale).orient("top");
+// orient("top") 代表數字將會出現在axis的上方
+var yAxis = d3.svg.axis().scale(yScale).orient("left");
+// 目前axis只被造出來，還沒放到頁面上
+// g => group element，可以把它看成一個container，用來放我們造出來的axis
+canvas.append("g")
+        .attr({
+          "class": "axis",
+          transform: "translate("+[0, margin.top]+")"
+        }).call(xAxis);
+canvas.append("g")
+        .attr({
+          "class": "axis",
+          transform: "translate("+[margin.left, 0]+")"
+        }).call(yAxis);
+// Create circles
 
 canvas.selectAll("circle")
       .data(data)
